4478be8a20db5304b91a9ff61c0016d7f23c6505
YashBitCoder <131183150+YashBitCoder@users.noreply.github.com>
2025-02-14 20:53:44 +0530

    Update fetchData.js

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -3,5 +3,5 @@ export default async function handler(req, res) {
     const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
     const data = await response.json();
     
-    res.status(200).json(data);
+    return res.status(200).json(data);
 }
