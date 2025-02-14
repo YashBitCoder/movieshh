fc867966b2008d9c4cc1e5d0b819d704464ac047
YashBitCoder <131183150+YashBitCoder@users.noreply.github.com>
2025-02-14 21:05:05 +0530

    Update fetchData.js

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,7 +1,3 @@
 export default async function handler(req, res) {
-    const API_KEY = process.env.SECRET_API_KEY; // Securely stored on the backend
-    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
-    const data = await response.json();
-    
-    return res.status(200).json(data);
+    new Response('Hello World!', { status: 200 })
 }
