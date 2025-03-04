4ada8d4dc781e533d60083a2081ae6de6a11a543
YashBitCoder <131183150+YashBitCoder@users.noreply.github.com>
2025-03-04 21:54:53 +0530

    Update fetchBase.js

--- api/fetchBase.js
+++ api/fetchBase.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const API_KEY = process.env.SECRET_API_KEY; // Securely stored on the backend
-    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+    const response = await fetch(`https://api.themoviedb.org/3/search/person?query=john&api_key=${API_KEY}&page=1`);
     const data = await response.json();
     
     res.status(200).json(data);
